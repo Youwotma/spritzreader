967b95efcbba0420b5d406dad3a02ed670beb206
David Bengoa <david@bengoarocandio.com>
2016-07-24 16:34:47 +0200

    PEP8

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from config import FEEDLY_CLIENT_ID, FEEDLY_SANDBOX, FEEDLY_REDIRECT_URI, FEEDLY
 import time
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    req = request
     if "code" in request.args:
         feedly = get_feedly_client()
         res_access_token = feedly.get_access_token(FEEDLY_REDIRECT_URI, request.args['code'])
@@ -18,9 +18,10 @@ def hello():
         return redirect("/")
 
     if "access" not in session:
-        return "You are not logged in. <a href='/auth'>Login with feedly</a>";
+        return "You are not logged in. <a href='/auth'>Login with feedly</a>"
 
     return send_file("./static/index.html")
+
 
 def get_feedly_client():
     if "access" in session:
@@ -56,15 +57,20 @@ def feeds():
     fl = get_feedly_client()
     return jsonify(res=fl.get_user_subscriptions(fl.token))
 
+
 @app.route("/feed")
 def feed():
     fl = get_feedly_client()
-    return jsonify(res=fl.get_feed_content(fl.token, "user/" + session['id'] + "/category/global.all", request.args.to_dict()))
+    url = "user/" + session['id'] + "/category/global.all"
+    return jsonify(res=fl.get_feed_content(fl.token, url, request.args.to_dict()))
+
 
 @app.route("/starred")
 def starred():
     fl = get_feedly_client()
-    return jsonify(res=fl.get_feed_content(fl.token, "user/" + session['id'] + "/tag/global.saved", request.args.to_dict()))
+    url = "user/" + session['id'] + "/tag/global.saved"
+    return jsonify(res=fl.get_feed_content(fl.token, url, request.args.to_dict()))
+
 
 @app.route("/settokens", methods=['POST'])
 def settoken():
@@ -72,20 +78,24 @@ def settoken():
     session['access'] = request.form['token']
     return redirect("/")
 
+
 @app.route("/mark_read", methods=["POST"])
 def mark_read():
     fl = get_feedly_client()
     return jsonify(res=fl.mark_article_read(fl.token, [request.args['item']]).status_code)
+
 
 @app.route("/star", methods=["POST"])
 def star():
     fl = get_feedly_client()
     return jsonify(res=fl.save_for_later(fl.token, session['id'], request.args['item']).status_code)
 
+
 @app.route("/unstar", methods=["POST"])
 def unstar():
     fl = get_feedly_client()
     return jsonify(res=fl.save_for_later(fl.token, session['id'], request.args['item'], False).status_code)
+
 
 @app.route("/auth")
 def auth():
@@ -94,6 +104,12 @@ def auth():
     code_url = feedly.get_code_url(FEEDLY_REDIRECT_URI)
     return redirect(code_url)
 
+
+@app.route("/proxy")
+def proxy():
+    return ""
+
+
 @app.route("/logout")
 def logout():
     for k in 'access', 'refresh', 'expires', 'id':
@@ -101,8 +117,7 @@ def logout():
             del session[k]
     return redirect("/")
 
-app.secret_key='yZ7BMXMOXj7YA3cR2yS0WrNPll8tilaEBiiRjreRKJB389orNqqwGE='
+app.secret_key = 'yZ7BMXMOXj7YA3cR2yS0WrNPll8tilaEBiiRjreRKJB389orNqqwGE='
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080, host='0.0.0.0')
-
